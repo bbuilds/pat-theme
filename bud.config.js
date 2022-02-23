54583108d1d00d147c468df36a90da43ae774c6e
Branden Youngs <hello@brandenbuilds.com>
2022-02-23 14:20:09 -0500

    initial commit

---
+++ bud.config.js
@@ -0,0 +1,45 @@
+/**
+ * @typedef {import('@roots/bud').Bud} bud
+ *
+ * @param {bud} app
+ */
+module.exports = async (app) => {
+  app
+    /**
+     * Application entrypoints
+     *
+     * Paths are relative to your resources directory
+     */
+    .entry({
+      app: ['@scripts/app', '@styles/app'],
+      editor: ['@scripts/editor', '@styles/editor'],
+    })
+
+    /**
+     * These files should be processed as part of the build
+     * even if they are not explicitly imported in application assets.
+     */
+    .assets('images')
+
+    /**
+     * These files will trigger a full page reload
+     * when modified.
+     */
+    .watch([
+      'tailwind.config.js',
+      'resources/views/**/*.blade.php',
+      'app/View/**/*.php',
+    ])
+
+    /**
+     * Target URL to be proxied by the dev server.
+     *
+     * This is your local dev server.
+     */
+    .proxy('http://powerassettech.local')
+
+    /**
+     * Development URL
+     */
+    .serve('http://powerassettech.local:3000');
+};
