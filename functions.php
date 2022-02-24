fa4dedc6ac4751240264c8255c695648e9760120
Branden Youngs <hello@brandenbuilds.com>
2022-02-24 08:49:50 -0500

    feat: add ACF options

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ try {
 |
 */
 
-collect(['setup', 'filters'])
+collect(['setup', 'filters', 'ACF/options'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
