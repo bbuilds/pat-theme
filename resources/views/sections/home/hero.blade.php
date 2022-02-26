e2521943eebeaa28c76795b5a92a1503954c58e7
Branden Youngs <hello@brandenbuilds.com>
2022-02-25 20:46:00 -0500

    feat: added home hero section

---
+++ resources/views/sections/home/hero.blade.php
@@ -0,0 +1,10 @@
+<section id="home=hero">
+
+  <div class="container mx-auto px-4 py-12">
+    <header>
+      <h1 class="sr-only">{{$title}}</h1>
+      <h2 class="mb-6 md:w-7/12">Powerful insider IT experts providing reliable and secure technical solutions that help propel the real estate ecosystem forward.</h2>
+    </header>
+    <a href="/contact-us" class="button">Contact Us</a>
+  </div>
+</section>
