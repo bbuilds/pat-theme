60835338dd6fa4ed3ccd538ceaec0240935c425b
Branden Youngs <hello@brandenbuilds.com>
2022-03-13 13:51:40 -0400

    installed rankmath + added breadcrumbs to blog header

--- resources/views/partials/blog-header.blade.php
+++ resources/views/partials/blog-header.blade.php
@@ -1,10 +1,11 @@
-<div class="page-header py-10 lg:py-20">
+<header class="page-header py-10 lg:py-20">
   <div class="container px-4 mx-auto lg:px:0">
-    <div class="flex items-center md:justify-between">
+    <div class="flex items-center md:justify-between mb-6">
       <h1>{!! $title !!}</h1>
       <div class="md:w-2/5">
        {!! get_search_form(false) !!}
       </div>
     </div>
+    <?php if (function_exists('rank_math_the_breadcrumbs')) rank_math_the_breadcrumbs(); ?>
   </div>
-</div>
+</header>
